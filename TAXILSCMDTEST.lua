d9d22edfa1e99dc9e898e16215cf4b7419c232e5
deejayxoxofficial-cloud <deejayxoxofficial@gmail.com>
2026-03-05 14:03:29 +0200

    Update TAXILSCMDTEST.lua

--- TAXILSCMDTEST.lua
+++ TAXILSCMDTEST.lua
@@ -2,7 +2,7 @@ local requests = require 'requests'
 local sampev = require 'lib.samp.events'
 
 -- CONFIGURARE AUTO-UPDATE
-local script_version = 2.3
+local script_version = 2.4
 local last_update = "05/03/2026 - 13:40"
 local script_name = "TAXICMD"
 -- Link-urile update(cu anti-cache inclus direct)
@@ -430,4 +430,5 @@ function checkUpdate()
             end
         end
     end)
+
 end
